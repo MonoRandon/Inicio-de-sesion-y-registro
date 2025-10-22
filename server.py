b49dcb212d08ec47bf1cc7bb2a98de6343fb468d
MonoRandon <162451933+MonoRandon@users.noreply.github.com>
2025-10-22 08:18:17 -0300

    Update server.py
    mala posicion de port

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 from __init__ import create_app
+from app.config.mysqlconnection import MySQLConnection
 
 app = create_app()
 
-port= 5022
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5022)
